66c50643c05ece25a8032d4b1e9e69ff3f7ecf2a
20212pf.cc0012 <20212pf.cc0012@pc-207538.passofundo.ifsul.local>
2024-03-25 09:12:19 -0300

    exercicio1094

---
+++ src/exerciciosAula/exercicio1094.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exerciciosAula;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class exercicio1094 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
