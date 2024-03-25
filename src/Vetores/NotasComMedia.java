0afe1a2f437c4e0dd7fc21fef6b3e5ccb224917a
Carlos Augusto Segala Aguirre <carlos.aguirre@be8energy.com>
2024-03-25 16:38:44 -0300

    exercicio media notas 2 maneiras

--- src/Vetores/NotasComMedia.java
+++ src/Vetores/NotasComMedia.java
@@ -3,18 +3,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package Vetores;
+
+import java.util.Scanner;
 //Pedir para o usuário inserir valores para adiconar ao vetor, percorrer o vetor e realizar uma média dos valores armazenados no vetor;
-/**
- *
- * @author carlos.aguirre
- */
+
 public class NotasComMedia {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Scanner entrada = new Scanner(System.in);
+       
+        int vet[] = new int[10];
+        int total = 0;
+        float media;
+        
+        for (int i = 0; i < vet.length; i++) {
+            vet[i] = entrada.nextInt();
+            total = total + vet[i];
+        }
+        
+        media = (float) total / vet.length;
+        
+        System.out.printf("A media eh: %.2f\n", media);
     }
-    
 }
---
+++ src/Vetores/NotasComMedia1.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Vetores;
+import javax.swing.JOptionPane;
+
+public class NotasComMedia1 {
+
+    public static void main(String[] args) {
+        
+        String entrada = JOptionPane.showInputDialog("Informe quantas notas deseja armazenar: ");
+        int quant = Integer.parseInt(entrada);
+        int vet[] = new int[quant];
+        int total = 0;
+        float media;
+        
+        for (int i = 0; i < vet.length; i++) {
+            String notaStr = JOptionPane.showInputDialog("Informe a " + (i + 1) + "ª nota: ");
+            int nota = Integer.parseInt(notaStr);
+            vet[i] = nota;
+            total = total + nota;
+        }
+        
+        media = (float)total/quant;
+        
+        JOptionPane.showMessageDialog(null, String.format("A media das notas eh: %.2f", media));
+        
+        
+    }
+    
+}
