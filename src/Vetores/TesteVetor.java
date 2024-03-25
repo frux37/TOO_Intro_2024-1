e2781b152970ba91ab954c7e716876927556f915
Carlos Augusto Segala Aguirre <carlos.aguirre@be8energy.com>
2024-03-25 14:32:22 -0300

    commitado

---
+++ src/Vetores/TesteVetor.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Vetores;
+
+/**
+ *
+ * @author carlos.aguirre
+ */
+public class TesteVetor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] vetor = new int[6];
+        int vetor2[] = new int[6];
+        
+        
+        //int a[], b, c;
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println("vetor ["+i+"] = "+vetor[i]);
+        }
+    }
+    
+}
