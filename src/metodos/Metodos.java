0e1b3a4788b2f826e8a96d64acd4bb3419d29622
20212pf.cc0012 <20212pf.cc0012@pc-155961.passofundo.ifsul.local>
2024-04-08 10:38:29 -0300

    teste commit

---
+++ src/metodos/Metodos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package metodos;
+
+/**
+ *
+ * @author 20212pf.cc0012
+ */
+public class Metodos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("SEXO");
+    }
+    
+}
